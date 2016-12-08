060b13d2b55773b18d8034be5b8d4304a38d4522
David G <david.a.grs@gmail.com>
2016-12-08 22:45:12 +0100

    using mtrace<malloc_chrono>

--- bench.cc
+++ bench.cc
@@ -15,13 +15,19 @@ static constexpr const int Iterations = 4e6;
 void benchmark()
 {
     std::unordered_map<int, double> um;
+    mtrace<malloc_chrono> mt;
 
     auto start = std::chrono::system_clock::now();
     for (int i = 0; i < Iterations; ++i)
       um.emplace(i, i);
     auto end = std::chrono::system_clock::now();
 
-    std::cout << "took " << (end - start).count() << "ns" << std::endl;
+    malloc_chrono chr = std::get<0>(mt);
+
+    std::cout << "took " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+    std::cout << "time spent in malloc(): " << std::chrono::duration_cast<std::chrono::milliseconds>(chr.malloc_time()).count() << "ms" << std::endl;
+    std::cout << "time spent in free(): " << std::chrono::duration_cast<std::chrono::milliseconds>(chr.free_time()).count() << "ms" << std::endl;
+    std::cout << "time spent in realloc(): " << std::chrono::duration_cast<std::chrono::milliseconds>(chr.realloc_time()).count() << "ms" << std::endl;
 }
 
 void other_benchmarks()
@@ -40,14 +46,18 @@ void other_benchmarks()
 
 int main()
 {
+    std::cout << "running benchmark first time... ";
     benchmark();
     // std::cout << "after first benchmark:" << std::endl;
     // malloc_info(0, stdout);
 
+    std::cout << "running some other benchmarks...";
     other_benchmarks();
+    std::cout << "OK" << std::endl;
 
     // std::cout << "after second benchmark:" << std::endl;
     // malloc_info(0, stdout);
+    std::cout << "running first benchmark a second time... ";
     benchmark();
 
     return 0;

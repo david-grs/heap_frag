0b498efbce2be0c938388427858f49813f953220
David G <david.a.grs@gmail.com>
2016-12-28 19:35:54 +0100

    clean-up

--- bench.cc
+++ bench.cc
@@ -1,8 +1,6 @@
 #include <mtrace/mtrace.h>
 #include <mtrace/malloc_chrono.h>
 
-#include <papipp.h>
-
 #include <unordered_map>
 #include <vector>
 #include <map>
@@ -14,22 +12,19 @@
 
 static constexpr const int Iterations = 4e6;
 
-void benchmark_unordered_map_emplace(papi::event_set<PAPI_TOT_INS, PAPI_TOT_CYC>& events)
+void benchmark_unordered_map_emplace()
 {
     std::unordered_map<int, int> um;
     mtrace<malloc_chrono> mt;
 
     auto start = std::chrono::system_clock::now();
-    events.reset_counters();
     for (int i = 0; i < Iterations; ++i)
       um.emplace(i, i);
-    events.reset_counters();
     auto end = std::chrono::system_clock::now();
 
     malloc_chrono chr = std::get<0>(mt);
 
     std::cout << Iterations << " iterations of unordered_map<int, int>::emplace took " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
-    std::cout << events.at<0>().counter() << " instructions, " << events.at<0>().counter() << " cycles" << std::endl;
     std::cout << "time spent in malloc(): " << std::chrono::duration_cast<std::chrono::milliseconds>(chr.malloc_time()).count() << "ms" << std::endl;
     std::cout << "time spent in free(): " << std::chrono::duration_cast<std::chrono::milliseconds>(chr.free_time()).count() << "ms" << std::endl;
     std::cout << "time spent in realloc(): " << std::chrono::duration_cast<std::chrono::milliseconds>(chr.realloc_time()).count() << "ms" << std::endl;
@@ -51,10 +46,7 @@ void other_benchmarks()
 
 int main()
 {
-    papi::event_set<PAPI_TOT_INS, PAPI_TOT_CYC> events;
-    events.start_counters(); // start_counters() is more expensive, we call it once, then reset_counters
-
-    benchmark_unordered_map_emplace(events);
+    benchmark_unordered_map_emplace();
     // std::cout << "after first benchmark:" << std::endl;
     // malloc_info(0, stdout);
 
@@ -62,7 +54,9 @@ int main()
 
     // std::cout << "after second benchmark:" << std::endl;
      malloc_info(0, stdout);
-    benchmark_unordered_map_emplace(events);
+     malloc_info(0, stdout);
+
+    benchmark_unordered_map_emplace();
 
     return 0;
 }
